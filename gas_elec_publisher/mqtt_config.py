55b4b352fd2ccdf4c36bed7cfe26a3189e480459
Clifford B. Brown <cbrown350@users.noreply.github.com>
2023-10-20 07:27:33 +0000

    Fix indentation

--- gas_elec_publisher/mqtt_config.py
+++ gas_elec_publisher/mqtt_config.py
@@ -11,30 +11,30 @@ HA_CONFIG_PAYLOAD_ENERGY = '''
                     "topic": "{AVAILABLE_TOPIC}"
                 }} 
             ],
-            "availability_mode": "all",
-            "device":
-                {{
-                    "identifiers": [ "{UNIQUE_ID}_{meter_type}" ],
-                    "manufacturer": "{VENDOR_NAME}",
-                    "model": "RTL SDR wireless electrical power and gas company meter reader ({DEVICE_ID})",
-                    "name": "Main {meter_type_CAP} Meter Reader",
-                    "sw_version": "{SW_VERSION}"
-                }},
-            "device_class": "{meter_device_class}",
-            "icon": "{meter_icon}",
-            "enabled_by_default": true,
-            "object_id": "main_{meter_type}_meter_energy",
-            "origin":
-                {{
-                    "name": "{VENDOR_ID}",
-                    "sw": "{SW_VERSION}",
-                    "url": "{REPO}"
-                }},
-            "state_class": "total_increasing",
-            "state_topic": "{STATE_TOPIC}",
-            "unique_id": "{UNIQUE_ID}_{meter_type}_energy",
-            "unit_of_measurement": "{meter_units}",
-            "value_template": "{{{{ value_json.energy }}}}"}}
+        "availability_mode": "all",
+        "device":
+            {{
+                "identifiers": [ "{UNIQUE_ID}_{meter_type}" ],
+                "manufacturer": "{VENDOR_NAME}",
+                "model": "RTL SDR wireless electrical power and gas company meter reader ({DEVICE_ID})",
+                "name": "Main {meter_type_CAP} Meter Reader",
+                "sw_version": "{SW_VERSION}"
+            }},
+        "device_class": "{meter_device_class}",
+        "icon": "{meter_icon}",
+        "enabled_by_default": true,
+        "object_id": "main_{meter_type}_meter_energy",
+        "origin":
+            {{
+                "name": "{VENDOR_ID}",
+                "sw": "{SW_VERSION}",
+                "url": "{REPO}"
+            }},
+        "state_class": "total_increasing",
+        "state_topic": "{STATE_TOPIC}",
+        "unique_id": "{UNIQUE_ID}_{meter_type}_energy",
+        "unit_of_measurement": "{meter_units}",
+        "value_template": "{{{{ value_json.energy }}}}"}}
     '''
 
 HA_CONFIG_TOPIC_LINKQUALITY = "homeassistant/sensor/{UNIQUE_ID}/{meter_type}_{meter_id}_linkquality/config"
@@ -46,31 +46,31 @@ HA_CONFIG_PAYLOAD_LINKQUALITY = '''
                     "topic": "{AVAILABLE_TOPIC}"
                 }} 
             ],
-            "availability_mode": "all",
-            "device":
-                {{
-                    "identifiers": [ "{UNIQUE_ID}_{meter_type}" ],
-                    "manufacturer": "{VENDOR_NAME}",
-                    "model": "RTL SDR wireless electrical power and gas company meter reader ({DEVICE_ID})",
-                    "name": "Main {meter_type_CAP} Meter Reader",
-                    "sw_version": "{SW_VERSION}"
-                }},
-            "enabled_by_default": true,
-            "entity_category": "diagnostic",
-            "icon": "mdi:signal",
-            "name": "Linkquality",
-            "object_id": "main_{meter_type}_meter_linkquality",
-            "origin":
-                {{
-                    "name": "{VENDOR_ID}",
-                    "sw": "{SW_VERSION}",
-                    "url": "{REPO}"
-                }},
-            "state_class": "measurement",
-            "state_topic": "{STATE_TOPIC}",
-            "unique_id": "{UNIQUE_ID}_{meter_type}_linkquality",
-            "unit_of_measurement": "dBm",
-            "value_template": "{{{{ value_json.linkquality }}}}"}}
+        "availability_mode": "all",
+        "device":
+            {{
+                "identifiers": [ "{UNIQUE_ID}_{meter_type}" ],
+                "manufacturer": "{VENDOR_NAME}",
+                "model": "RTL SDR wireless electrical power and gas company meter reader ({DEVICE_ID})",
+                "name": "Main {meter_type_CAP} Meter Reader",
+                "sw_version": "{SW_VERSION}"
+            }},
+        "enabled_by_default": true,
+        "entity_category": "diagnostic",
+        "icon": "mdi:signal",
+        "name": "Linkquality",
+        "object_id": "main_{meter_type}_meter_linkquality",
+        "origin":
+            {{
+                "name": "{VENDOR_ID}",
+                "sw": "{SW_VERSION}",
+                "url": "{REPO}"
+            }},
+        "state_class": "measurement",
+        "state_topic": "{STATE_TOPIC}",
+        "unique_id": "{UNIQUE_ID}_{meter_type}_linkquality",
+        "unit_of_measurement": "dBm",
+        "value_template": "{{{{ value_json.linkquality }}}}"}}
     '''
 
 HA_CONFIG_TOPIC_LASTSEEN = "homeassistant/sensor/{UNIQUE_ID}/{meter_type}_{meter_id}_last_seen/config"
@@ -82,28 +82,28 @@ HA_CONFIG_PAYLOAD_LASTSEEN = '''
                     "topic": "{AVAILABLE_TOPIC}"
                 }} 
             ],
-            "availability_mode": "all",
-            "device":
-                {{
-                    "identifiers": [ "{UNIQUE_ID}_{meter_type}" ],
-                    "manufacturer": "{VENDOR_NAME}",
-                    "model": "RTL SDR wireless electrical power and gas company meter reader ({DEVICE_ID})",
-                    "name": "Main {meter_type_CAP} Meter Reader",
-                    "sw_version": "{SW_VERSION}"
-                }},
-            "device_class": "timestamp",
-            "enabled_by_default": false,
-            "entity_category": "diagnostic",
-            "icon": "mdi:clock",
-            "name": "Last seen",
-            "object_id": "main_{meter_type}_meter_last_seen",
-            "origin":
-                {{
-                    "name": "{VENDOR_ID}",
-                    "sw": "{SW_VERSION}",
-                    "url": "{REPO}"
-                }},
-            "state_topic": "{STATE_TOPIC}",
-            "unique_id": "{UNIQUE_ID}_{meter_type}_last_seen",
-            "value_template": "{{{{ value_json.last_seen }}}}"}}
+        "availability_mode": "all",
+        "device":
+            {{
+                "identifiers": [ "{UNIQUE_ID}_{meter_type}" ],
+                "manufacturer": "{VENDOR_NAME}",
+                "model": "RTL SDR wireless electrical power and gas company meter reader ({DEVICE_ID})",
+                "name": "Main {meter_type_CAP} Meter Reader",
+                "sw_version": "{SW_VERSION}"
+            }},
+        "device_class": "timestamp",
+        "enabled_by_default": false,
+        "entity_category": "diagnostic",
+        "icon": "mdi:clock",
+        "name": "Last seen",
+        "object_id": "main_{meter_type}_meter_last_seen",
+        "origin":
+            {{
+                "name": "{VENDOR_ID}",
+                "sw": "{SW_VERSION}",
+                "url": "{REPO}"
+            }},
+        "state_topic": "{STATE_TOPIC}",
+        "unique_id": "{UNIQUE_ID}_{meter_type}_last_seen",
+        "value_template": "{{{{ value_json.last_seen }}}}"}}
     '''
